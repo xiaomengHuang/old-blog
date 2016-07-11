93cd0597a1a1d6d8fbbb66caba631deb7f144a20
kevin <1@1.com>
2016-07-11 21:33:07 +0800

    上传数据

---
+++ js/service.js
@@ -0,0 +1,35 @@
+/**
+ * Created by kevin on 16-7-11.
+ */
+!function(){
+
+    var app = angular.module('myApp', []);
+    app.controller('myCtrl', function($scope) {
+        $scope.article = [];
+        var git = new Github({username:"526371075@qq.com",password:"q2670187",auth:"basic"});
+        var repo = git.getRepo("noobfan-kevin","noobfan-kevin.github.io");
+        var options = {
+            author: {name: 'kevin', email: 'author@example.com'},
+            committer: {name: 'kevin', email: 'committer@example.com'},
+            encode: true // Whether to base64 encode the file. (default: true)
+        };
+
+         new Promise(function(resolve,reject){
+             repo.read('master', 'dataFile/indexData.json', function(err, data) {
+                 var __data__ = JSON.parse(data);
+                 resolve(__data__);
+
+             });
+         }).then(function(info){
+             for(x in info){
+                 var data = {};
+                 data.title = info[x].artName;
+                 data.detail = info[x].artDetail;
+                 $scope.article.push(data);
+             }
+             $scope.$apply($scope.article);
+         }).catch(function(err){
+             console.log(err);
+         });
+    });
+}();
