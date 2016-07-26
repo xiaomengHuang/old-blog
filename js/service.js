d7411e548fe49deea097e4970bbed027c464f640
kevin <1@1.com>
2016-07-26 15:34:22 +0800

    文章详情

--- js/service.js
+++ js/service.js
@@ -243,7 +243,26 @@
             console.log('life page');
             }]);
 
-    app.controller('detailCtrl',['$scope', function($scope){
+    app.controller('detailCtrl',['$scope','Repo', function($scope,Repo){
+        $scope.detailInfo = {};
+        Repo.getRepoObj().read('master',sessionStorage.getItem('detailUrl'), function(err, data) {
+            $scope.detailInfo={};
+            var __data__ = JSON.parse(data);
+            $scope.detailInfo.title = __data__.title;
+            $scope.detailInfo.time = __data__.time;
+            $scope.detailInfo.editor = __data__.editor;
+            $scope.detailInfo.type = __data__.type;
+            $scope.detailInfo.detail = [];
+            var imgList = __data__.img.split('##');
+            var detail = __data__.detail.split('##');
+            var _data = {};
+            for(var i=0;i<detail.length>imgList.length?detail.length:imgList.length;i++){
+                _data.detail = detail[i];
+                _data.img = imgList[i]||'';
+                $scope.detailInfo.detail.push(_data);
+            }
+            console.log( $scope.detailInfo);
+        });
         console.log('detail page');
         console.log(sessionStorage.getItem('detailUrl'));
     }]);
